206151b105a677dc945020723d7c4394a25f20d7
nagaraju <245122749001@mvsrec.edu.in>
2025-06-17 15:17:55 +0530

    Add files via upload

---
+++ Arithmetic.java
@@ -0,0 +1,13 @@
+public class Arithmetic
+{
+	public static void main(String args[])
+	{
+		int a=20;
+		int b=10;
+		System.out.println("sum="+(a+b));
+		System.out.println("subtraction="+(a-b));
+		System.out.println("multiplication="+(a*b));
+		System.out.println("Division="+(a/b));
+		System.out.println("Remainder="+(a%b));
+	}
+}
